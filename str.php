5d164152768d175dcc4b91a805b656ebea738d8c
jiangshilin <jiangshilin@ttyun.com>
2017-06-12 15:30:42 +0800

    提交几个函数

--- str.php
+++ str.php
@@ -1,6 +1,7 @@
 <?php
 $number = 23131234.5;
 echo '以千位分组来格式化数字number_format:'.number_format($number, 2);
-echo '<br>把字符串填充为新的长度str_pad:'.str_pad($number,20,0,STR_PAD_LEFT)
-echo '<br>str_split:'.str_split($number);
+echo '<br>把字符串填充为新的长度str_pad:'.str_pad($number,20,0,STR_PAD_LEFT);
+echo '<br>把字符串分割到数组中str_split:';var_dump(str_split($number));
+echo '<br>比较两个字符串0 - 如果两个字符串相等<0 - 如果 string1 小于 string2>0 - 如果 string1 大于 string2 strcmp:'.strcmp('Hel','hell');
 ?>
